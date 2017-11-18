95056512d2ad64a0d554113b38f0e6e840a64d8a
Muhammet Enginar <enginaryum@gmail.com>
2017-11-17 20:29:00 -0800

    cors package added

--- app.js
+++ app.js
@@ -4,16 +4,15 @@ const schema = require('./schema');
 const resolvers = require('./resolvers');
 
 const app = express();
-app.use('/graphql', graphqlHTTP(
-async (
-  request,
-  response,
-  graphQLParams
-) => ({
-  schema,
-  rootValue: resolvers,
-  graphiql: true,
-})));
+const cors = require('cors');
+
+app.use('/graphql', cors(), graphqlHTTP(
+  async (request, response, graphQLParams) => ({
+    schema,
+    rootValue: resolvers,
+    graphiql: true,
+  })));
+
 
 app.listen(4000);
 console.log('Running a GraphQL API server at localhost:4000/graphql');
