6f3516c8c30403da1fbad5ae9b7dc32373b3a41c
Lucy K Tan <lucyktan@users.noreply.github.com>
2017-11-29 20:14:02 -0800

    Revert "make stops available in tryn-api (as is already defined in schema) (#7)"
    This reverts commit 4450dbcb30fef1c0b854fa402c6ebc5fb65b8d21.

--- resolvers.js
+++ resolvers.js
@@ -1,28 +1,12 @@
 const getPrimaryKeys = require('./helpers/getCassandraKeys');
 const executeQuery = require('./helpers/cassandraHelper');
-const getRouteObj = require('./helpers/formatData');
-
-const axios = require('axios');
-const config = require('./config');
-
-function getStopsFromRouteID(routeID) {
-    return axios.get(`/agencies/sf-muni/routes/${routeID}`, {
-      baseURL: config.restbusURL
-    })
-    .then((response) => {
-      const stopsObj = response.data;
-      return stopsObj;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-}
+const putVehiclesIntoRoutes = require('./helpers/formatData');
 
 const resolvers = {
     trynState: async (obj) => {
         const { agency, startTime, endTime = startTime, routes } = obj;
         const { vdate, vhour } = getPrimaryKeys(startTime, endTime)[0];
-        // console.log(obj);
+        console.log(obj);
 
         // TODO - get these from config file using agency name
         const keyspace = agency;
@@ -32,19 +16,11 @@ const resolvers = {
             `SELECT * FROM ${keyspace}.${vehicleTableName} WHERE vdate = ? AND vhour = ? AND vtime > ? AND vtime < ?`,
             [vdate, vhour, new Date(startTime - 7500), new Date(startTime - (-7500))],
         );
-
         const vehicles = response.rows;
-
+        console.log(vehicles);
         // there is only one state as we assume endTime was not provided
         const stateTime = (vehicles[0] || {}).vtime;
-
-        const routeIDs = new Set(vehicles.map(vehicle => vehicle.rid));
-        let stopsPromises = [];
-        routeIDs.forEach((routeID) => stopsPromises.push(getStopsFromRouteID(routeID)));
-        const stops = await Promise.all(stopsPromises);
-
-        const stateRoutes = getRouteObj(routeIDs, vehicles, stops);
-
+        const stateRoutes = putVehiclesIntoRoutes(vehicles);
         return {
             agency,
             startTime,
